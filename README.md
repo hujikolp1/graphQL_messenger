4c4312f134b37875c5cdbf0622a7275e68e12122
bhunjimko <haroldulrich@protonmail.com>
2021-08-30 18:59:03 -0700

    show other users

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Fullstack GraphQL Messenger 
 
+#### Technology Used
+* React.js
+* GraphQL
+* PostgreSQL
+* Sequelize 
+
 ## Installation and Running
 
+#### Client 
+* In main directory `cd client_apollo_react`
 * `npm install`
 * `npm start`
-* visit `http://localhost:3000`
+* Visit client at `http://localhost:3000`
 
+#### Server 
+* In main directory `cd server_apollo_express_postgresql`
+* `npm install`
+* `npm start`
+* Visit GraphQL PlayGround at `http://localhost:8000/graphql` 
--- client_apollo_react/src/components/ShowUsers/index.js
+++ client_apollo_react/src/components/ShowUsers/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import withSession from '../Session/withSession';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+export const GET_ALL_USERS = gql`
+  {
+    users {
+      username
+      id
+      email
+      role
+    } 
+  }
+`; 
 
 const ShowUsers = ({ session }) => (
   <div>
@@ -8,6 +21,48 @@ const ShowUsers = ({ session }) => (
       <h2>Other Users</h2>
       {session && session.me && <div> All Users Here </div>}
     </div>
+
+    <Query
+      query={GET_ALL_USERS}
+    > 
+      {({ data, loading, error, fetchMore }) => {
+
+        console.log('DATA: ', data.users)
+        if (!data) {
+          return (
+            <div>
+              There are no users ...
+            </div>
+          );
+        }
+        if (loading) {
+          return (
+            <div>
+              Working on it ...
+            </div>
+          );
+        }
+        if (error) {
+          return (
+            <div>
+              Error broke my page ...
+            </div>
+          );
+        }
+        if (data) {
+          return (
+            <div>
+              {/* {data.users[1].username} */}
+              {data.users.map(i => {
+                return <li>{i.username}</li>
+              })}
+            </div>
+            
+          );
+        }
+      }}
+    </Query>
+
 
   </div>
 );
